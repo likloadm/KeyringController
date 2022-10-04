b5dd45314f4f7c637e1c5c0064c782c6fb959f75
Vasilii <liklo.adm@gmail.com>
2022-10-04 19:42:57 +0300

    update package

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const encryptor = require('browser-passworder');
 const { normalize: normalizeAddress } = require('eth-sig-util');
 
 const SimpleKeyring = require('eth-simple-keyring');
-const HdKeyring = require('@metamask/eth-hd-keyring');
+const HdKeyring = require('eth-hd-keyring');
 
 const keyringTypes = [SimpleKeyring, HdKeyring];
 
